9f888911f661a97d6753454756eeb93e5a0c6a1c
Wanderson Camargo <wanderson.camargo@azion.com>
2021-07-20 15:21:58 -0300

    Prompt for MFA token code

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM alpine:3.14
 
-RUN apk add --no-cache aws-cli \
+RUN apk add --no-cache aws-cli coreutils jq \
     && adduser -u 1000 -g 1000 -D azion \
     && mkdir /home/azion/.aws \
     && chown 1000:1000 /home/azion/.aws
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,27 @@
 #!/usr/bin/env sh
 
-set -xe
+set -e
 
 if [ ! -f "/home/azion/.aws/credentials" ]; then
     aws configure
+fi
+
+if [ ! -f "/home/azion/.aws/identity.json" ]; then
+    aws sts get-caller-identity > "/home/azion/.aws/identity.json"
+fi
+
+SESSION_EXPIRATION=0
+if [ -f "/home/azion/.aws/session.json" ]; then
+    EXPIRATION=`jq --raw-output .Credentials.Expiration "/home/azion/.aws/session.json"`
+    SESSION_EXPIRATION=`date -d"$EXPIRATION" +%s`
+fi
+
+CURRENT_DATETIME=`date +%s`
+
+if [ "$SESSION_EXPIRATION" -le "$CURRENT_DATETIME" ]; then
+    read -p "MFA Token Code: " AWS_TOKEN_CODE
+    AWS_SERIAL_NUMBER=`jq --raw-output .Arn "/home/azion/.aws/identity.json" | sed 's/:user/:mfa/'`
+    aws sts get-session-token --serial-number "$AWS_SERIAL_NUMBER" --token-code "$AWS_TOKEN_CODE" > "/home/azion/.aws/session.json"
 fi
 
 aws $*
